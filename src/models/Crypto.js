6de758940743c486832ce16b9c4f5d3f45f286dc
Rui Pedro <ruipedro@Ruis-MacBook-Air.local>
2019-06-16 13:20:10 +0100

    first commit

--- src/models/Crypto.js
+++ src/models/Crypto.js
@@ -21,12 +21,6 @@ class Crypto{
         return Math.floor(Math.random() * 10000000000000000) + 1;
     }
 
-    generateRandomResult(server_seed, client_seed){
-        let randomHex = SHA512(server_seed, client_seed);
-        return randomHex;
-
-    }
-
     hexToInt = (randomHex) => {
         // TO DO : If this number is over 999,999 than the next 5 characters (ex : aad5e) would be used. But in our case it's 697,969 so this will be used. Now you only have to apply a modulus of 10^4 and divide it by 100
         let hexString = randomHex.toString().substring(0, 5);
